5f052c7ce41aef43e765b4e480c6d8fcadc5acb2
jjuhee <azaza.hee@gmail.com>
2023-12-24 15:48:25 +0900

    add my pic

--- countdown.js
+++ countdown.js
@@ -3,21 +3,24 @@ const targetDate = new Date("2023-12-25T00:00:00Z");
 
 function updateCountdown() {
   // 현재 한국 시간을 얻어오기
-  const now = new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Seoul" }));
-
+  const now = new Date(
+    new Date().toLocaleString("en-US", { timeZone: "Asia/Seoul" })
+  );
 
   // 남은 시간 계산
   const timeRemaining = targetDate - now;
 
   // 시간, 분, 초 계산
   const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const hours = Math.floor(
+    (timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
   const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
   const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
   // 결과를 HTML에 업데이트
-  const countdownElement = document.getElementById('countdown');
-  countdownElement.style.color = '#FF9EA9'
+  const countdownElement = document.getElementById("countdown");
+  countdownElement.style.color = "#FF9EA9";
   countdownElement.innerHTML = `
       <span>D-${days} ${hours}시간${minutes}분${seconds}초</span>`;
 }
@@ -28,11 +31,13 @@ updateCountdown();
 setInterval(updateCountdown, 1000);
 
 // 날짜 기준 카드 오픈 기능
-const doors = document.querySelectorAll('.door');
+const doors = document.querySelectorAll(".door");
 doors.forEach((door, index) => {
-  door.addEventListener('click', () => {
+  door.addEventListener("click", () => {
     // 현재 한국 시간을 얻어오기
-    const now = new Date(new Date().toLocaleString("en-US", { timeZone: "Asia/Seoul" }));
+    const now = new Date(
+      new Date().toLocaleString("en-US", { timeZone: "Asia/Seoul" })
+    );
 
     // 각 날짜에 해당하는 날짜를 계산
     const openDate = new Date(2023, 11, index + 1); // 2023년 12월 1일부터 시작
@@ -45,31 +50,32 @@ doors.forEach((door, index) => {
       const imageUrl = `image/card/card-${index + 1}.png`;
 
       // 'back' 클래스를 가진 요소를 찾아 스타일을 가져옵니다.
-      const doorDiv = document.querySelector(`.day-${index + 1}`)
+      const doorDiv = document.querySelector(`.day-${index + 1}`);
       const backDiv = doorDiv.querySelector(`.back`);
 
       const style = window.getComputedStyle(backDiv);
-      const pTag = backDiv.querySelector('p')
-      const text = modalMessageList[index]['message']
+      const pTag = backDiv.querySelector("p");
+      const text = modalMessageList[index]["message"];
 
-      const prevDoorInput = document.querySelector(`.day-${index} > label > input`)
-      const doorInput = document.querySelector(`.day-${index + 1} > label > input`)
+      const prevDoorInput = document.querySelector(
+        `.day-${index} > label > input`
+      );
+      const doorInput = document.querySelector(
+        `.day-${index + 1} > label > input`
+      );
 
-      if(index===0|| prevDoorInput.checked) {
+      if (index === 0 || prevDoorInput.checked) {
         // showModal 함수를 호출하여 모달을 표시합니다.
         showModal(imageUrl, text);
         // alert('이벤트 캘린더를 엽니다.');
       } else {
         doorInput.checked = false;
-        showModal("", "이전카드를 먼저 열어줘..",index);
-        
+        showModal("image/card/jjoo1.jpg", "이전카드를 먼저 열어줘..", index);
       }
-        
-
     } else {
       // 현재 날짜가 열 수 있는 날짜보다 이전인 경우 몇 일 후에 열 수 있다는 메시지를 표시
       const daysRemaining = Math.ceil((openDate - now) / (1000 * 60 * 60 * 24));
-      console.log(openDate, now, daysRemaining)
+      console.log(openDate, now, daysRemaining);
       alert(`이 카드는 ${daysRemaining}일 후에 열 수 있어요!`);
     }
   });
